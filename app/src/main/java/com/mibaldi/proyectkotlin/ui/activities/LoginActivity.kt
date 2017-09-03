415604345dbba486b6ad21a7e7a25275acbef50b
Pablo Jimenez Casado <pjimenez.casado@atsistemas.com>
2017-09-03 11:38:03 +0200

    Add Login Activity

---
+++ app/src/main/java/com/mibaldi/proyectkotlin/ui/activities/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.mibaldi.proyectkotlin.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.mibaldi.proyectkotlin.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
